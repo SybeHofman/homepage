4e92c0eadde8b0c714a7f6ddf9335604499c6553
Sybe Hofman <sybe.t.hofman@gmail.com>
2025-04-06 21:04:55 -0700

    Trying again

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/sybehofman.github.io/",
+  base: "/sybe-hofman-portfolio/",
   plugins: [react()],
 });
