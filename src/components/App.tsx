7d699c5d0fde58806614d88843274f6c3d5c0ea4
Sybe Hofman <sybe.hofman@outlook.com>
2025-08-19 12:14:16 -0700

    Moved some stuff around and removed nine square

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,43 @@
-import Landing from "./landing/Landing.tsx";
+import Intro from "./landing/Intro.tsx";
 import "./App.css"
+import NavBar from "./navBar/NavBar.tsx";
+import { useState } from "react";
 
 function App() {
+  const [inIntro, setInIntro] = useState<boolean>(true);
   return(
     <div className = "app">
-      <Landing/>
+      {inIntro ? <Intro inIntro = {inIntro} setInIntro = {setInIntro}/> :
+      <div className = "homepage">
+          <NavBar></NavBar>
+          <div className="content" id="about">
+            <h1>ABOUT ME</h1>
+            <ul>
+              <li>I am a high schoolstudent at North Creek High School</li>
+              <li>I am a discus thrower</li>
+              <li>I am a programmer</li>
+            </ul>
+            <div>That's about it!</div>
+          </div>
+          <div className="content" id="projects">
+            <h1>PROJECTS</h1>
+            <div>Will update in the future</div>
+          </div>
+          <div className="content" id="contacts">
+            <h1>CONTACTS</h1>
+            <div> 
+              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=sybe.t.hofman@gmail.com" target="_blank" rel="noopener noreferrer">
+                Email: sybe.t.hofman@gmail.com
+              </a>
+            </div>
+            <div>
+              <a href="https://discordapp.com/users/1183215061943205988">
+                Discord: enemy_starling47
+              </a>
+            </div>
+          </div>
+      </div>
+      }
     </div>
   );
 }
--- src/components/landing/Intro.tsx
+++ src/components/landing/Intro.tsx
@@ -1,16 +1,75 @@
 import "./Intro.css";
+import { useState, useEffect } from "react";
 
-interface Intro {
+
+interface ShowText {
     text: string;
 }
 
-function Intro ({text}: Intro) {
+interface Intro {
+    inIntro: boolean;
+    setInIntro: (intro: boolean) => void;
+}
+
+function ShowText ({text}: ShowText) {
     return(
-        <div className="wrapper center">
-            <div className="cursor-text">
-                 {text}
-                 <span className="cursor"></span>
+        <div className="intro">
+            <div className="wrapper center">
+                <div className="cursor-text">
+                    {text}
+                    <span className="cursor"></span>
+                </div>
             </div>
+        </div>
+    )
+}
+
+function Intro({inIntro, setInIntro}: Intro) {
+    //TODO: remove animation for redirects
+    const [index, setIndex] = useState(0);
+
+    const text = "C:\\CoolGuys> cd Syeb-w-d-dbe Hofman"
+    const speed = 200;
+
+    const getIntroText = (i: number) => {
+        let s = "";
+        for(let j = 0; j <= i; j++){
+            if(text.charAt(j) === '-'){
+                if(text.charAt(j + 1) === 'w'){
+                    j++;
+                }
+                else if(text.charAt(j + 1) === 'd'){
+                    s = s.slice(0, -1);
+                    j++;
+                }
+            } else{
+                s += text.charAt(j);
+            }
+        }
+        return s;
+    }
+
+    useEffect(() => {
+        if (!inIntro) return;
+
+        if (index < text.length) {
+            const intervalId = setInterval(() => {
+                setIndex((prev) => prev + 1);
+            }, speed);
+
+            return () => clearInterval(intervalId);
+        } else {
+            // Animation finished, show NavBar after a short delay (optional)
+            const timeoutId = setTimeout(() => setInIntro(false), 500);
+            return () => clearTimeout(timeoutId);
+        }
+    }, [index, inIntro, text.length, speed]);
+
+
+
+    return(
+        <div>
+            <ShowText text = {getIntroText(index)} />
         </div>
     )
 }
